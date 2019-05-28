421146f8ade28761140b6f4da20e1cf08dc956d5
Borja <50354622+borjasen1996@users.noreply.github.com>
2019-05-28 12:11:38 +0200

    Add files via upload

---
+++ src/refactorizar/NodoPila.java
@@ -0,0 +1,13 @@
+package refactorizar;
+
+class NodoPila {
+	public NodoPila abajo;
+	public double dato;
+
+	public NodoPila(double dato, NodoPila abajo) {
+		this.dato = dato;
+		this.abajo = abajo;
+	}
+}
+
+
---
+++ src/refactorizar/RPN.java
@@ -0,0 +1,70 @@
+package refactorizar;
+
+public class RPN {
+	
+	private String commando;
+	private NodoPila arriba;
+	
+	public void pushPila(double nuevo_dato) {
+		NodoPila nuevo_nodo = new NodoPila(nuevo_dato, arriba);
+		arriba = nuevo_nodo;
+	}
+	public double popPila( ) { 
+		double dato_arriba = arriba.dato;
+		arriba = arriba.abajo;
+		return dato_arriba;
+	}
+	public RPN(String commando) {
+		arriba = null;
+		this.commando = commando;
+	}
+	public double resultado( ) {
+		double a, b;
+		for(int i= 0; i< commando.length( ); i++) {
+			// si es un digito
+			if(Character.isDigit(commando.charAt(i))) {
+				double numero;
+				String temp = "";
+				for(int j= 0; (j< 100) && (Character.isDigit(commando.charAt(i)) || (commando.charAt(i) == '.')); j++, i++) {
+					temp = temp + String.valueOf(commando.charAt(i));
+				}
+				// convertira double y añadira la pila
+				numero = Double.parseDouble(temp);
+				pushPila(numero);
+			} 
+			else if(commando.charAt(i) == '+') {
+				b = popPila( );
+				a = popPila( );
+				pushPila(a+ b);
+			} else if(commando.charAt(i) == '-') {
+				b = popPila( );
+				a = popPila( );
+				pushPila(a-b);
+			} else if(commando.charAt(i) == '*') {
+				b = popPila( );
+				a = popPila( );
+				pushPila(a* b);
+			} else if(commando.charAt(i) == '/') {
+				b = popPila( );
+				a = popPila( );
+				pushPila(a/ b);
+			}else if(commando.charAt(i) == '^') {
+				b = popPila( );
+				a = popPila( );
+				pushPila(Math.pow(a, b));
+			}else if(commando.charAt(i) == '%') {
+				b = popPila( );
+				a = popPila( );
+				pushPila(a%b);
+			} else if(commando.charAt(i) != ' ') {
+				throw new IllegalArgumentException( );
+			}
+		}
+		double val= popPila( );
+		if(arriba != null) {
+			throw new IllegalArgumentException( );
+		}
+		return val;
+	}
+	
+}
